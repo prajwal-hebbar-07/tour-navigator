48c06dffbe02c71b3aab5221af1230926a914651
Hebbar <prajwalhebbaras@gmail.com>
2024-08-03 12:28:28 +0530

    feat(ui): add asic layout for the home page
    - Added a basic layout for the Home Page.
    - Used the data.json to map through the array of objects to display the title of the city.
    - Then mapped through the tours which is the child of the city object to display different available tours.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+import cities from "../data.json";
 
 const Home = () => {
   return (
     <>
-      <Typography variant="h1">Home Page</Typography>
+      <Container maxWidth="md">
+        {cities.map((city) => (
+          <Box key={city.id}>
+            <Typography
+              variant="h3"
+              component="h2"
+              color="secondary"
+              fontWeight="bold"
+              mb={3}
+            >
+              {city.name}
+            </Typography>
+            <Grid container spacing={2}>
+              {city.tours.map((tour) => (
+                <p>{tour.name}</p>
+              ))}
+            </Grid>
+          </Box>
+        ))}
+      </Container>
     </>
   );
 };
