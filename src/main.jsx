d57871eba13e857e2b1679b37074c01ff18e18fa
Hebbar <prajwalhebbaras@gmail.com>
2024-08-03 09:54:52 +0530

    feat(theme): add a basic theme & ThemeProvider form MUI
    - Added a basic theme using the createTheme function from MUI.
    - Wrapped the App component inside the ThemeProvider and Suspense elements.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createTheme, ThemeProvider } from "@mui/material";
+
+import App from "./App.jsx";
+
+import "./index.css";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#244855",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#e64833",
+      contrastText: "#fff",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </ThemeProvider>
+  </React.StrictMode>
+);
